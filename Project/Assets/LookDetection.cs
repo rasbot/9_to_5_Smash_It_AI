df89470bbd4c2c8119a4cf73fed6e17890911ba6
RasNath <nathan.f.rasmussen@gmail.com>
2020-06-08 11:57:08 -0700

    add updated Unity files

---
+++ Project/Assets/LookDetection.cs
@@ -0,0 +1,36 @@
+﻿
+using UnityEngine;
+
+public class LookDetection : MonoBehaviour
+{
+    PuncherAgent puncher;
+
+    bool isLooking=false;
+
+    void Start()
+    {
+        puncher = GetComponentInParent<PuncherAgent>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Target")
+        {
+            isLooking = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.name == "Target")
+        {
+            isLooking = false;
+        }
+    }
+
+
+    void FixedUpdate()
+    {
+        puncher.setIsLooking(isLooking);
+    }
+}
---
+++ Project/Assets/testScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        float result = 0;
+        float value = 10*Random.value;
+        if (value >= 5)
+		{
+            result = 1;
+		}
+        print(result);
+    }
+}

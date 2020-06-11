90d53555079addff7b88c2e115506848606105e9
RasNath <nathan.f.rasmussen@gmail.com>
2020-06-11 10:01:18 -0700

    update agent script

--- Project/Assets/PuncherAgent.cs
+++ Project/Assets/PuncherAgent.cs
@@ -7,7 +7,9 @@ using System.Runtime.InteropServices;
 
 public class PuncherAgent : Agent
 {
+	public bool isTesting = false;
 	public float torque = 20;
+	public float speed = 5;
 	public bool isDebugging = false;
 
 	public GameObject Target;
@@ -22,25 +24,27 @@ public class PuncherAgent : Agent
 
 	bool isLookingTarget;
 	public int lookCount;
+	float height;
 
 
 	void Start()
     {
-		anim = Puncher.GetComponent<Animator>();
-        rBody = GetComponent<Rigidbody>();
+		height = gameObject.transform.position.y;
+        anim = Puncher.GetComponent<Animator>();
+		rBody = GetComponent<Rigidbody>();
 		targetrBody = Target.GetComponent<Rigidbody>();
 		rBody.isKinematic=true;
 	}
 
     public override void OnEpisodeBegin()
     {
-		// y = 1.0 for cubebot and 2.04 for agentbot
-        transform.localPosition = new Vector3(0, 2.04f, 0);
+        transform.localPosition = new Vector3(0, height, 0);
 
-        float spawnArea = 9.0f;
-        float minTargetDist = 2f;
+        float spawnArea = 13f;
+        float minTargetDist = 5f;
 
         Vector3 pos = Vector3.zero;
+		Vector3 objPos = Vector3.zero;
 
 		if (isDebugging)
 		{
@@ -48,24 +52,32 @@ public class PuncherAgent : Agent
 			{
 				pos = new Vector3(Random.Range(-1f, 1), 0, Random.Range(-1f, 1)) * spawnArea;
 				if (pos.magnitude < minTargetDist) pos = pos.normalized * minTargetDist; // Clamp to minTargetDist if too close
-				new GameObject().transform.position = pos;//DEBUG TEST
+				objPos = new Vector3(pos.x - gameObject.transform.position.x, pos.y, pos.z - gameObject.transform.position.z);
+				GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+				sphere.transform.position = objPos;
 			}
 		}
 		else
 		{
 			pos = new Vector3(Random.Range(-1f, 1), 0, Random.Range(-1f, 1)) * spawnArea;
 			if (pos.magnitude < minTargetDist) pos = pos.normalized * minTargetDist; // Clamp to minTargetDist if too close
+			objPos = new Vector3(pos.x - gameObject.transform.position.x, pos.y, pos.z - gameObject.transform.position.z);
 		}
 
 
-		Target.transform.localPosition = pos + Vector3.up * 0.5f; // set at pos and lift upward a bit
+		Target.transform.position = objPos + Vector3.up * 2.57f; // set at pos and lift upward a bit
     }
 
 	public override void CollectObservations(VectorSensor sensor)
 	{
-		sensor.AddObservation(Target.transform.position - gameObject.transform.position);
-		sensor.AddObservation(targetrBody.velocity.x);
-		sensor.AddObservation(targetrBody.velocity.z);
+		if (Target != null) 
+		{
+			sensor.AddObservation(Target.transform.position - gameObject.transform.position);
+			sensor.AddObservation(targetrBody.velocity.x);
+			sensor.AddObservation(targetrBody.velocity.z);
+		}
+		//sensor.AddObservation(rBody.velocity.x);
+		//sensor.AddObservation(rBody.velocity.z);
 		sensor.AddObservation(isHittingTarget);
 		sensor.AddObservation(hitCount);
 		sensor.AddObservation(lookCount);
@@ -77,7 +89,12 @@ public class PuncherAgent : Agent
 		// Actions
 		if (vectorAction[1] > 0)
 		{
-			anim.SetTrigger("Punch");  
+			anim.SetTrigger("Punch");
+		}
+
+		if (isLookingTarget && vectorAction[1] > 0)
+		{
+
 		}
 
 		Vector3 controlSignal = Vector3.zero;
@@ -86,19 +103,21 @@ public class PuncherAgent : Agent
 		Quaternion rot = transform.rotation;
 		transform.Rotate(controlSignal);
 		transform.rotation = Quaternion.Slerp( transform.rotation, rot, torque * Time.fixedDeltaTime);
-		//
-		
+
 
 		// End State
-		if (hitCount > 4) DestroyedTarget();
-		//
+		if (hitCount > 4)
+		{
+			//StartCoroutine(Explosion());
+			DestroyedTarget();
+			EndEpisode();
+		}
 	}
 
 	void DestroyedTarget()
 	{
-		AddReward(1.0f);
-		EndEpisode();
-
+		AddReward(10.0f);
+		//EndEpisode();
 		hitCount = 0;
 		isHittingTarget = false;
 		
@@ -106,22 +125,13 @@ public class PuncherAgent : Agent
 		isLookingTarget = false;
 	}
 
-	void OnTriggerEnter(Collider other)
-	{
-		if (other.tag == "Target")
-		{
-			print("Bot hit Agent!");
-		}
-	}
-
 	public void setIsHitting(bool newState)
 	{
 		isHittingTarget = newState;
 
 		if(isHittingTarget) {
-			print("hit detected");
 			hitCount++;
-			AddReward(0.2f);
+			AddReward(1.0f);
 		}
 	}
 
@@ -136,7 +146,7 @@ public class PuncherAgent : Agent
 		} 
 		else
 		{
-			AddReward( -(1 / 60f) );
+			AddReward( -(2 / 60f) );
 		}
 	}
 }

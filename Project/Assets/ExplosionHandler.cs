c2ba1f46dcde3a9552ae884806b4922aaf9aeac1
RasNath <nathan.f.rasmussen@gmail.com>
2020-06-11 09:59:40 -0700

    add explosion handler script

---
+++ Project/Assets/ExplosionHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionHandler : MonoBehaviour
+{
+	public GameObject explosions;
+	public GameObject walker;
+	public PuncherAgent puncher;
+
+    void Start()
+    {
+        explosions.SetActive(false);
+		puncher = GameObject.FindGameObjectWithTag("Puncher").GetComponent<PuncherAgent>();
+
+    }
+
+	IEnumerator Explosion()
+	{
+		explosions.SetActive(true);
+		yield return new WaitForSeconds(2.0f);
+		explosions.SetActive(false);
+		walker.SetActive(false);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("Puncher"))
+			StartCoroutine(Explosion());
+	}
+}
